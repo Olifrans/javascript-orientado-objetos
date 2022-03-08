54615b8c6e2b2788d0d00902b1d6aea42ea8d8cf
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-03-08 12:23:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// import { Cliente } from "./Cliente";
+// import { ContaCorrente } from "./ContaCorrente";
+
+
+
+
+
+
+const cliente1Nome = new Cliente("Olifrans", 7771627);
+const cliente1CPF = 77187558418;
+const cliente1Agencia = 101;
+const cliente1Saldo = 0;
+
+
+const cliente2Nome = new Cliente("Jack", 2657771627);
+const cliente2CPF = 5785875588;
+const cliente2Agencia = 101;
+const cliente2Saldo = 0;
+
+
+console.log(cliente1Nome);
+
+
+
+
+
+
+
+
+
+
+// const contaContaCorrenteOlifrans = new ContaCorrente(1001, cliente1);
+
+// contaContaCorrenteOlifrans.depositar(777);
+// contaContaCorrenteOlifrans.sacar(77);
+
+// const conta2 = new ContaCorrente(102, cliente2);
+
+// let valor = 127;
+// contaContaCorrenteOlifrans.transferir(valor, conta2);
+
+// console.log(contaContaCorrenteOlifrans);
